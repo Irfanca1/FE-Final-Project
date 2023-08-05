b20c1e8e196086c162fd02ac903bfb03e4c66ef9
irfanca1 <irfanchoiruddin23@gmail.com>
2023-08-05 18:21:40 +0700

    integrasi API home

--- components/Home/index.jsx
+++ components/Home/index.jsx
@@ -1,6 +1,4 @@
-// HalamanUtama.js
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-datepicker/dist/react-datepicker.css';
 import styles from '@/styles/Home.module.css';
@@ -8,20 +6,37 @@ import { Row, Col, InputGroup, FormControl, Button } from 'react-bootstrap';
 import { GiAirplaneDeparture, GiAirplaneArrival } from 'react-icons/gi';
 import { BsFillCalendarDateFill } from 'react-icons/bs';
 import { MdAirlineSeatReclineExtra, MdHotelClass } from 'react-icons/md';
-import { BsToggleOff, BsToggleOn } from 'react-icons/bs';
-import { AiOutlineSwap } from 'react-icons/ai';
+import { BsToggleOff, BsToggleOn, BsSearch } from 'react-icons/bs';
+import { AiOutlineSwap, AiOutlineArrowRight } from 'react-icons/ai';
 import DatePicker from 'react-datepicker';
 import { registerLocale } from 'react-datepicker';
 import id from 'date-fns/locale/id';
+import axios from 'axios';
 
 registerLocale('id', id);
 
 const HalamanUtama = () => {
-  const [selectedDate, setSelectedDate] = React.useState(null);
-  const [selectedDateReturn, setSelectedDateReturn] = React.useState(null);
-  const [isRoundtrip, setIsRoundtrip] = React.useState(false);
-  const [fromLocation, setFromLocation] = React.useState('Choose...');
-  const [toLocation, setToLocation] = React.useState('Choose...');
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedDateReturn, setSelectedDateReturn] = useState(null);
+  const [isRoundtrip, setIsRoundtrip] = useState(false);
+  const [fromLocation, setFromLocation] = useState('Choose...');
+  const [toLocation, setToLocation] = useState('Choose...');
+  const [dataDestinations, setDataDestinations] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/api/home');
+        const destinations = response.data.data;
+        console.log(destinations);
+        setDataDestinations(destinations);
+      } catch (error) {
+        console.log('Error fetching destinations:', error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
@@ -119,7 +134,7 @@ const HalamanUtama = () => {
                     <InputGroup>
                       <InputGroup.Text>
                         <MdAirlineSeatReclineExtra className="me-3" />
-                        Penumpang
+                        Passenge
                       </InputGroup.Text>
                     </InputGroup>
                   </Col>
@@ -127,7 +142,7 @@ const HalamanUtama = () => {
                     <InputGroup>
                       <InputGroup.Text>
                         <MdHotelClass className="me-3" />
-                        Kelas Kursi
+                        Seat Class
                       </InputGroup.Text>
                     </InputGroup>
                   </Col>
@@ -156,6 +171,50 @@ const HalamanUtama = () => {
         <Button className="fw-bold" style={{ backgroundColor: 'rgb(147, 6, 147)', borderColor: 'rgb(182, 24, 182)' }}>
           Cari Penerbangan
         </Button>
+      </div>
+      <div className={`fw-bold ${styles.destination} ${styles.description}`}>
+        <div className="fs-2">Destination Favorite</div>
+        <div className="d-flex p-2">
+          <Button className="m-2 fw-bold" style={{ backgroundColor: 'rgb(147, 6, 147)', borderColor: 'rgb(182, 24, 182)' }}>
+            <BsSearch className="me-2" /> Semua
+          </Button>
+          <Button className="m-2 fw-bold" style={{ backgroundColor: 'rgb(147, 6, 147)', borderColor: 'rgb(182, 24, 182)' }}>
+            <BsSearch className="me-2" /> Asia
+          </Button>
+          <Button className="m-2 fw-bold" style={{ backgroundColor: 'rgb(147, 6, 147)', borderColor: 'rgb(182, 24, 182)' }}>
+            <BsSearch className="me-2" /> Amerika
+          </Button>
+          <Button className="m-2 fw-bold" style={{ backgroundColor: 'rgb(147, 6, 147)', borderColor: 'rgb(182, 24, 182)' }}>
+            <BsSearch className="me-2" /> Australia
+          </Button>
+          <Button className="m-2 fw-bold" style={{ backgroundColor: 'rgb(147, 6, 147)', borderColor: 'rgb(182, 24, 182)' }}>
+            <BsSearch className="me-2" /> Eropa
+          </Button>
+          <Button className="m-2 fw-bold" style={{ backgroundColor: 'rgb(147, 6, 147)', borderColor: 'rgb(182, 24, 182)' }}>
+            <BsSearch className="me-2" /> Afrika
+          </Button>
+        </div>
+        <Row className="mt-5">
+          {dataDestinations.map((destination) => (
+            <Col md={4} key={destination.id}>
+              <div className="card" style={{ width: '18rem' }}>
+                <img src={destination.foto} height={'300px'} alt="Rumah Adat" />
+                <div className="card-body">
+                  <h5 className="card-title fs-6">
+                    {destination.bandaraAwal.kota} <AiOutlineArrowRight /> {destination.bandaraTujuan.kota}
+                  </h5>
+                  <h6>{destination.maskapai.nama_maskapai}</h6>
+                  <h6>
+                    Mulai dari <span className="text-danger fw-bold">{destination.maskapai.harga_tiket}</span>
+                  </h6>
+                </div>
+                <Button className="m-2 fw-bold" style={{ backgroundColor: 'rgb(147, 6, 147)', borderColor: 'rgb(182, 24, 182)' }}>
+                  Pilih
+                </Button>
+              </div>
+            </Col>
+          ))}
+        </Row>
       </div>
     </div>
   );
